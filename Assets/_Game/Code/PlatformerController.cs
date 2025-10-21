190dccf3f6a232929faaacac11fb2a1793e33ea6
Seth McArdle <McArdle-S17@ulster.ac.uk>
2025-10-21 15:49:14 +0100

    Added Animation

--- Assets/_Game/Code/PlatformerController.cs
+++ Assets/_Game/Code/PlatformerController.cs
@@ -1,58 +1,80 @@
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class PlatformerController : MonoBehaviour
 {
     [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 12f;
-    
+
+
+
     [Header("Ground Check")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius = 0.2f;
     [SerializeField] private LayerMask groundLayer;
-    
+    [SerializeField] bool isFlippedX = false;
+
     private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sr;
     private bool isGrounded;
     private float moveInput;
-    
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+
         // Set to Dynamic with gravity
         rb.bodyType = RigidbodyType2D.Dynamic;
         rb.gravityScale = 3f;
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
-    
+
     void Update()
     {
         // Get horizontal input
         moveInput = Input.GetAxisRaw("Horizontal");
-        
+
         // Check if grounded
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-        
+
         // Jump input
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
     }
-    
+
     void FixedUpdate()
     {
         // Apply horizontal movement
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-    }
-    
-    // Visualise ground check in editor
-    void OnDrawGizmosSelected()
-    {
-        if (groundCheck != null)
+        if (rb.linearVelocityY != 0)
         {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+            anim.SetBool("isWalking", true);
+            if (rb.linearVelocityY > 0)
+            {
+                sr.flipX = false;
+                isFlippedX = false;
+            }
+            else
+            {
+                sr.flipX = true;
+                isFlippedX = true;
+            }
+        }
+
+        // Visualise ground check in editor
+        void OnDrawGizmosSelected()
+        {
+            if (groundCheck != null)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+            }
         }
     }
 }
