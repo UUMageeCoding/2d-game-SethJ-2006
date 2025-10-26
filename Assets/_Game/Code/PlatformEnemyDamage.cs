894a83a8ca18ccd03ed472f264d29fbc00eb9e9d
SethJ-2006 <mcardle-s17@ulster.ac.uk>
2025-10-26 13:40:52 +0000

    Added tiles

--- Assets/_Game/Code/PlatformEnemyDamage.cs
+++ Assets/_Game/Code/PlatformEnemyDamage.cs
@@ -3,19 +3,13 @@ using UnityEngine;
 public class PlatformEnemyDamage : MonoBehaviour
 {
     public int damage;
-    public PlayerHealthRed playerHealthR;
-    public PlayerHealthBlue playerHealthB;
+    public PlatformCharacterHealth playerHealthR;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player Red")
         {
             playerHealthR.TakeDamage(damage);
-        }
-
-        if (collision.gameObject.tag == "Player Blue")
-        {
-            playerHealthB.TakeDamage(damage);
         }
     }
 }
