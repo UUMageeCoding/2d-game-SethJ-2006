2b916f2c30855c865159f1987320da80be608077
SethJ-2006 <mcardle-s17@ulster.ac.uk>
2025-10-26 13:10:24 +0000

    Added Health code

---
+++ Assets/_Game/Code/PlatformCharacterHealth.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlatformCharacterHealth : MonoBehaviour
+{
+    public int maxHealth = 1;
+    public int redHealth;
+
+    public bool isDead;
+
+    public GameManagerScript gameManager;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        redHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        redHealth = damage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (redHealth <= 0 && !isDead)
+        {
+            isDead = true;
+            gameObject.SetActive(false);
+            gameManager.gameOver();
+            Debug.Log("You Are Dead");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D ether)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
