e5a350690e09ca1aeeaeb95e795461c1f55ad7d2
SethJ-2006 <mcardle-s17@ulster.ac.uk>
2025-10-26 14:03:40 +0000

    Update PlatformEnemyDamage.cs

--- Assets/_Game/Code/PlatformEnemyDamage.cs
+++ Assets/_Game/Code/PlatformEnemyDamage.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlatformEnemyDamage : MonoBehaviour
 {
-    public int damage;
+    public int damage = 1;
     public PlatformCharacterHealth playerHealthR;
 
     private void OnCollisionEnter2D(Collision2D collision)
