94c31cd049399bf5f6c748afe30582f03cb0d933
Seth McArdle <McArdle-S17@ulster.ac.uk>
2025-11-11 14:31:34 +0000

    Created Death Zone script

--- Assets/_Game/Code/PlatformInstaDeath.cs
+++ Assets/_Game/Code/PlatformInstaDeath.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlatformInstaDeath : MonoBehaviour
+
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public int Respawn;
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(Respawn);
+        }
     }
 }
