c7bf3bb1e1ae08c58759bf7479c9c29bbee7443a
Seth McArdle <McArdle-S17@ulster.ac.uk>
2025-11-11 11:32:26 +0000

    Update BigBossAI.cs

--- Assets/_Game/Code/BigBossAI.cs
+++ Assets/_Game/Code/BigBossAI.cs
@@ -10,18 +10,23 @@ public enum BigBossState
 
 public class BigBossAI : MonoBehaviour
 {
+    public Transform playerPosition;
+    int hysteresis = 2;
+    SpriteRenderer spriteRenderer;
+
+    public int attackDistance;
     // Current state of the Big Boss
     public BigBossState currentState = BigBossState.Idle;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        float distanceToPlayer = Vector2.Distance(transform.position, playerPosition.position);
     }
 }
