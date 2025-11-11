dbbbed1f881c8eebbef69a90663c5757cb374e07
Seth McArdle <mcardle-s17@ulster.ac.uk>
2025-11-11 11:08:07 +0000

    Update WeeEnemy.cs

--- Assets/_Game/Code/WeeEnemy.cs
+++ Assets/_Game/Code/WeeEnemy.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class WeeEnemy : MonoBehaviour
 {
     public Transform playerPosition;
+    Vector2 directionToPlayer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerPosition = null;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // get direction to player
+        if (playerPosition != null)
+        {
+                
+                directionToPlayer = (playerPosition.position - transform.position).normalized;
+        Debug.Log(directionToPlayer);
+        }
     }
 }
