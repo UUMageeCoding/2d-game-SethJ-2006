6d49b4304dcba7c88362e44c3cfbb97d74073543
Seth McArdle <mcardle-s17@ulster.ac.uk>
2025-11-11 10:53:37 +0000

    In class task

---
+++ Assets/_Game/Code/WeeEnemy.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class WeeEnemy : MonoBehaviour
+{
+    public Transform playerPosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerPosition = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
