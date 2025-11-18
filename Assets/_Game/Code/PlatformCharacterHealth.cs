645bee3194e66a90f388badea16a319142c8bb46
Seth McArdle <McArdle-S17@ulster.ac.uk>
2025-11-18 10:35:48 +0000

    Working on damage

--- Assets/_Game/Code/PlatformCharacterHealth.cs
+++ Assets/_Game/Code/PlatformCharacterHealth.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlatformCharacterHealth : MonoBehaviour
 {
-    public int maxHealth = 1;
+    public int maxHealth = 10;
     public int playerHealth;
 
     public bool isDead;
