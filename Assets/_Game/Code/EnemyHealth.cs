48f378939052a0b796136043be05a07eb1e9b859
SethJ-2006 <mcardle-s17@ulster.ac.uk>
2025-11-22 14:47:21 +0000

    Added enemy health script

---
+++ Assets/_Game/Code/EnemyHealth.cs
@@ -0,0 +1,10 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float maxHealth = 5;
+    public float currentHealth;
+
+}
