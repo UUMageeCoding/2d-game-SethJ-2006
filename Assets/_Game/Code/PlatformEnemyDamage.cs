eed0403c09c2758542fe0c981b66f921caf2499c
SethJ-2006 <mcardle-s17@ulster.ac.uk>
2025-11-22 14:19:43 +0000

    Added enemy knockback

--- Assets/_Game/Code/PlatformEnemyDamage.cs
+++ Assets/_Game/Code/PlatformEnemyDamage.cs
@@ -4,11 +4,21 @@ public class PlatformEnemyDamage : MonoBehaviour
 {
     public int damage;
     public PlatformCharacterHealth playerHealth;
+    public PlatformerController playerMovement;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            playerMovement.knockbackCounter = playerMovement.knockbackTotalTime;
+            if (collision.transform.position.x <= transform.position.x)
+            {
+                playerMovement.knockbackFromRight = true;
+            }
+            if(collision.transform.position.x >= transform.position.x)
+            {
+                playerMovement.knockbackFromRight = false;
+            }
             playerHealth.TakeDamage(damage);
         }
     }
