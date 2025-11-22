c890a4bf380eea2f03181114b84a564fc8526f77
SethJ-2006 <mcardle-s17@ulster.ac.uk>
2025-11-22 16:17:07 +0000

    Update PlatformAttackHitbox.cs

--- Assets/_Game/Code/PlayerControls/PlatformAttackHitbox.cs
+++ Assets/_Game/Code/PlayerControls/PlatformAttackHitbox.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
 
 public class PlatformAttackHitbox : MonoBehaviour
+
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   public int damage = 1;
+   public EnemyHealth currentHealth;
+   private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.CompareTag("Enemy"))
+        {
+            currentHealth.TakeDamage(damage);
+        }
     }
 }
