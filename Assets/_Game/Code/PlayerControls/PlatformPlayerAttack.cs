ae8bb00c72a0561a83b6052832a4133ede87b322
SethJ-2006 <mcardle-s17@ulster.ac.uk>
2025-11-22 16:04:27 +0000

    Update PlatformPlayerAttack.cs

--- Assets/_Game/Code/PlayerControls/PlatformPlayerAttack.cs
+++ Assets/_Game/Code/PlayerControls/PlatformPlayerAttack.cs
@@ -3,14 +3,34 @@ using UnityEngine;
 public class PlatformPlayerAttack : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Animator animator;
+    private BoxCollider2D hitbox;
+    
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        hitbox =
+    transform.Find("HitboxGameObjectName").GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.LeftShift)) // Attacks when the Left Shift key is pressed.
+        {
+            animator.SetTrigger("Player_Attack_Animation");
+            Invoke("ActivateHitbox", 12f);
+            Invoke("DeactiveHitbox", 20f);
+        }
+    }
+
+    void ActivateHitbox()
+    {
+        hitbox.gameObject.SetActive(true);
+    }
+
+    void DeactiveHitbox()
+    {
+        hitbox.gameObject.SetActive(false);
     }
 }
