6c87f1a9ad34bc6d739cb0be61479b34bbc9749f
SethJ-2006 <mcardle-s17@ulster.ac.uk>
2025-11-23 17:57:33 +0000

    Update EnemyPatrolScript.cs

--- Assets/_Game/Code/EnemyPatrolScript.cs
+++ Assets/_Game/Code/EnemyPatrolScript.cs
@@ -15,7 +15,7 @@ public class EnemyPatrolScript : MonoBehaviour
             transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
             if(Vector2.Distance(transform.position, patrolPoints[0].position) < 0.2f)
             {
-                transform.localScale = new Vector3(1, 1, 1);
+                // transform.localScale = new Vector3(1, 1, 1);
                 patrolDestination = 1;
             }
             if(patrolDestination == 1)
@@ -23,7 +23,7 @@ public class EnemyPatrolScript : MonoBehaviour
             transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
             if(Vector2.Distance(transform.position, patrolPoints[1].position) < 0.2f)
             {
-                transform.localScale = new Vector3(-1, 1, 1);
+                // transform.localScale = new Vector3(-1, 1, 1);
                 patrolDestination = 0;
             }
         }
