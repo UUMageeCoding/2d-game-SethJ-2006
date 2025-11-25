74ebdf29e422086ee0922fe6714ff633b6366a92
Seth McArdle <McArdle-S17@ulster.ac.uk>
2025-11-25 11:19:25 +0000

    Patrol script fixed thanks to Darryl.

--- Assets/_Game/Code/EnemyPatrolScript.cs
+++ Assets/_Game/Code/EnemyPatrolScript.cs
@@ -2,6 +2,42 @@ using UnityEngine;
 
 public class EnemyPatrolScript : MonoBehaviour
 {
+     public float enemySpeed = 1f;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    private bool isFacingRight = false; // Moves to the left first.
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+
+        rb.freezeRotation = true; // Won't rotate onto its side.
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float direction = isFacingRight ? 1f : -1f; // 1 = right, -1 = left
+        rb.linearVelocity = new Vector2(direction * enemySpeed, rb.linearVelocityY);
+
+        // Flip sprite via SpriteRender; can be commented out.
+        sr.flipX = !isFacingRight; // flipX = true when facing left
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        // When an enemy hits a 'solid wall', reverse direction
+        if (col.tag == "Solid")
+        {
+            isFacingRight = !isFacingRight;
+        }
+    }
+    
+}
+
+    /*
     public Transform[] patrolPoints;
     public float moveSpeed;
     public int patrolDestination;
@@ -20,9 +56,10 @@ public class EnemyPatrolScript : MonoBehaviour
                 localScale.x *= -1;
                 transform.localScale = localScale;
                 patrolDestination = 1;
+                transform.position += new Vector3(0.3f, 0, 0);
                 Debug.Log("Boing");
             }
-            if(patrolDestination == 1)
+        if(patrolDestination == 1)
         {
             transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
             if(Vector2.Distance(transform.position, patrolPoints[1].position) < 0.2f)
@@ -31,10 +68,11 @@ public class EnemyPatrolScript : MonoBehaviour
                 Vector3 localScale = transform.localScale;
                 localScale.x *= -1;
                 transform.localScale = localScale;
-                    patrolDestination = 0;
-                    Debug.Log("Bounce");
+                patrolDestination = 0;
+                transform.position += new Vector3(-0.3f, 0, 0);
+                Debug.Log("Bounce");
             }
         }
         }
     }
-}
+    */
