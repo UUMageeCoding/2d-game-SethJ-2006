6f1827a9b8f9bf269b1787cac2d5656344f03c39
SethJ-2006 <mcardle-s17@ulster.ac.uk>
2025-11-30 14:32:12 +0000

    Created secondary Attack Script

---
+++ Assets/_Game/Code/PlayerControls/PlayerAttackScript.cs
@@ -0,0 +1,41 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerAttackScript : MonoBehaviour
+
+{
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+    public Transform attackPos;
+    public LayerMask whatIsEnemy;
+    public float attackRange;
+    public int damage;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeBtwAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<EnemyHealth>().currentHealth -= damage;
+                }
+            }
+        
+        timeBtwAttack = startTimeBtwAttack;
+    } else
+    {
+        timeBtwAttack -= Time.deltaTime;
+    }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.ghostWhite;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
