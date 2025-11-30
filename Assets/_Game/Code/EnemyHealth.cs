8c942bcd2049160d015ebe66020daee3657d56c2
SethJ-2006 <mcardle-s17@ulster.ac.uk>
2025-11-30 15:55:05 +0000

    Update EnemyHealth.cs

--- Assets/_Game/Code/EnemyHealth.cs
+++ Assets/_Game/Code/EnemyHealth.cs
@@ -1,7 +1,7 @@
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class EnemyHealth : MonoBehaviour
+public class EnemyHealth : MonoBehaviour, IDamageable
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private float maxHealth;
