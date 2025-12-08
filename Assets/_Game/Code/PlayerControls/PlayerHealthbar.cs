5f09da64b442d57ed13dec29429341970ac66d1a
Seth McArdle <McArdle-S17@ulster.ac.uk>
2025-12-08 15:10:13 +0000

    Update PlayerHealthbar.cs

--- Assets/_Game/Code/PlayerControls/PlayerHealthbar.cs
+++ Assets/_Game/Code/PlayerControls/PlayerHealthbar.cs
@@ -1,11 +1,20 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealthbar : MonoBehaviour
 {
+    public Slider healthSlider;
+    public TMP_Text healthBarText;
+    public PlatformCharacterHealth playerHealth;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player
+        healthSlider.value = CalculateSliderPercentage();
+        healthBarText.text = "HP " + _ + " / " + 
     }
 
     // Update is called once per frame
