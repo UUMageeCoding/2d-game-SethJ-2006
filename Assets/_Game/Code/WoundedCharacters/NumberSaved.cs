c8373cc40c2576dcaceb54a4a422bb4c945ad486
Seth McArdle <McArdle-S17@ulster.ac.uk>
2025-12-09 12:29:26 +0000

    Created a counter for saved characters.

--- Assets/_Game/Code/WoundedCharacters/NumberSaved.cs
+++ Assets/_Game/Code/WoundedCharacters/NumberSaved.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using TMPro;
+using Unity.Burst.Intrinsics;
 
 public class NumberSaved : MonoBehaviour
 {
+
+    public static NumberSaved instance;
+    public TMP_Text woundedText;
+    public int noSaved = 0;
+
+    void Awake()
+    {
+        instance = this;
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        woundedText.text = "NO. SAVED: " + noSaved.ToString() + " / 5";
     }
 
     // Update is called once per frame
-    void Update()
+    public void SavedCharacter(int v)
     {
-        
+        noSaved += v;
+        woundedText.text = "NO. SAVED: " + noSaved.ToString() + " / 5";
     }
 }
--- Assets/_Game/Code/WoundedCharacters/SaveWounded.cs
+++ Assets/_Game/Code/WoundedCharacters/SaveWounded.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 
 public class SaveWounded : MonoBehaviour
 {
+    public int value;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Destroy(gameObject);
+            NumberSaved.instance.SavedCharacter(value);
             Debug.Log("Mine Worker Saved.");
         }
     }
