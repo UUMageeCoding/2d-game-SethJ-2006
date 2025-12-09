14ec6f65bfd58c3beaa7f521c5a931ec523fef65
Seth McArdle <McArdle-S17@ulster.ac.uk>
2025-12-09 10:21:34 +0000

    Update SaveWoundedText.cs

--- Assets/_Game/Code/SaveWoundedText.cs
+++ Assets/_Game/Code/SaveWoundedText.cs
@@ -1,20 +1,37 @@
+using TMPro;
 using UnityEngine;
 
 public class SaveWoundedText : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Vector3 moveSpeed = new Vector3(0, 75, 0);
+    public float timeToFade = 1f;
+    private float timeElapsed;
+    private Color startColor;
     RectTransform textTransform;
+    TextMeshProUGUI textMeshPro;
 
 
     private void Awake()
     {
         textTransform = GetComponent<RectTransform>();
+        textMeshPro = GetComponent<TextMeshProUGUI>();
+        startColor = textMeshPro.color;
     }
 
     // Update is called once per frame
     private void Update()
     {
         textTransform.position += moveSpeed * Time.deltaTime;
+
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed < timeToFade)
+        {
+            float fadeAlpha = startColor.a * (1 - (timeElapsed / timeToFade));
+            textMeshPro.color = new Color(startColor.r, startColor.g, startColor.b, fadeAlpha);
+        } else
+        {
+            Destroy(gameObject);
+        }
     }
 }
