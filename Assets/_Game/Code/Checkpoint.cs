c2d9463a813abf55fa9f64b9df4847ca3066df68
SethJ-2006 <mcardle-s17@ulster.ac.uk>
2025-12-12 16:01:57 +0000

    Updated Player Controller to include respawn script & started on checkpoint script.

--- Assets/_Game/Code/Checkpoint.cs
+++ Assets/_Game/Code/Checkpoint.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    PlatformerController platformerController;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        platformerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlatformerController>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            platformerController.UpdateCheckpoint(transform.position);
+            Debug.Log("Checkpoint reached!");
+        }
     }
 
     // Update is called once per frame
