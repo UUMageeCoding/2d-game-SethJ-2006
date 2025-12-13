9cf6afcb1a9c8a927d0cf9d83fe785b4634f8e42
SethJ-2006 <mcardle-s17@ulster.ac.uk>
2025-12-13 15:29:53 +0000

    Started trying to fix respawn & checkpoint system.

--- Assets/_Game/Code/Checkpoint.cs
+++ Assets/_Game/Code/Checkpoint.cs
@@ -7,7 +7,7 @@ public class Checkpoint : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        platformerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlatformerController>();
+        // platformerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlatformerController>();
     }
 
     void OnTriggerEnter2D(Collider2D collision)
